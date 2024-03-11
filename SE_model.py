4bba834684d4c5e5ce4274f31fb0bf12555dc41d
Wook0111 <101158250+Wook0111@users.noreply.github.com>
2024-03-11 14:39:43 +0900

    Create SE_model.py

---
+++ SE_model.py
@@ -0,0 +1,24 @@
+import os
+import pickle
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+from tqdm import tqdm
+from time import time
+from IPython import embed
+from collections import defaultdict
+from scipy.sparse import csc_matrix
+from transformers import AutoTokenizer, AutoConfig
+
+# Base Model import
+# utils import
+
+class SE_model():
+  def __init__(self, dataset, config, device):
+      self.dataset = dataset
+      self.model_conf = config
+      self.device = device
+
+  def get_sparse_output():
